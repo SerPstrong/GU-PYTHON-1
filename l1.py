e67568496f50b4d52da2c216fa1b84cdb4fc62a3
serpstrong <ommuts@gmail.com>
2020-04-15 23:04:41 +0500

    l1

---
+++ l1.py
@@ -0,0 +1,24 @@
+# 1
+# name = input("Введите ваше имя: ")
+# age = int(input("Введите ваш возраст: "))
+# year = 2020 - age
+# height = int(input("Введите ваш рост: "))
+# print("Привет", name, year, "года рождения")
+
+# 2
+# number = int(input("Введите кол-во секунд: "))
+# hour = number // 3600
+# minutes = (number // 60) % 60
+# sec = number % 60
+# print(hour, "ч", minutes, "м", sec, "с")
+
+# 3
+# n = input("Введите число: ")
+# n1 = int(n)
+# n2 = int(n + n)
+# n3 = int(n + n + n)
+# print(n1 + n2 + n3)
+
+# 4
+# 5
+# 6
