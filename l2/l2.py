bf5ba9e6bc40cd663a3c3157db17518a5c934221
serpstrong <ommuts@gmail.com>
2020-04-19 22:27:51 +0500

    l2

---
+++ l2/l2.py
@@ -0,0 +1,22 @@
+# 1
+int_var = int(15)
+n_1 = complex(5, 6)
+my_str = "простая строка"
+tup_var = (tuple('обычная строка'))
+abanchof = {400, None, "text", True}
+my_dict = dict(key_1='val_1', key_2='val_2')
+bool_var = bool()
+bytes_var = bytes([10, 20, 30, 40])
+none_var = None
+
+list_arr = [int_var, n_1, my_str, tup_var, abanchof, my_dict, bool_var, bytes_var, none_var]
+
+print(list_arr)
+
+print(len(list_arr))
+
+# 2
+# 3
+# 4
+# 5
+# 6
