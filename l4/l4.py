8dc07f25eb13e880e70d1366386473fba0e9dd5b
serpstrong <ommuts@gmail.com>
2020-04-27 21:51:23 +0500

    d

--- l4/l4.py
+++ l4/l4.py
@@ -1,10 +1,16 @@
-import func
 import w1
-
-func.show_msg()
-print(func.simple_calc())
 
 # 1
 print(w1.show_msg(2, 4, 60))
 
 # 2
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+new_list = [elem - 10 for elem in my_list]
+print(f"Исходный список: {my_list}")
+print(f"Новый список: {new_list}")
+
+# 3
+# 4
+# 5
+# 6
+# 7
