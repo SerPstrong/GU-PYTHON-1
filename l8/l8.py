5ae402b63bdf8edc94effd97911c69435df5b1ab
Sergey <ommuts@gmail.com>
2020-05-12 09:07:45 +0500

    l8

---
+++ l8/l8.py
@@ -0,0 +1,31 @@
+# 1
+class Data:
+    @classmethod
+    def date1(cls, x):
+        return int(x)
+
+    # def date2(self):
+
+
+a = "123"
+
+# print(a + 1)
+b = Data.date1(a)
+
+print(b+1)
+# 2
+
+
+# 3
+
+
+# 4
+
+
+# 5
+
+
+# 6
+
+
+# 7
